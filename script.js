b87c3b2c84c3ac82e2a83f06b090542358eab171
murphy-afk <bardarenoemi@gmail.com>
2025-10-27 14:58:16 +0100

    feat: human code

---
+++ script.js
@@ -0,0 +1,5 @@
+// Chiedere all'utente il numero di chilometri da percorrere e l'età
+// moltiplicare il numero di chilometri per la tariffa (0.21)
+// se l'età dell'utente è tra i 18 e i 65 stampare prezzo biglietto
+// se l'utente ha meno di 18 anni calcolare lo sconto del 20% e poi stampare il prezzo del biglietto
+// se l'utente ha più di 65 anni calcolare lo sconto del 40% e poi stampare il prezzo del biglietto
